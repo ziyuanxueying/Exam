61ca50d9f92055bdf3178d075edc412942ba0bba
ziyuanxueying <your_email@986852048@qq.com>
2016-02-24 11:20:53 +0800

    试试

--- src/com/exam/MainActivity.java
+++ src/com/exam/MainActivity.java
@@ -10,7 +10,6 @@ import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
 
 public class MainActivity extends FragmentActivity {
